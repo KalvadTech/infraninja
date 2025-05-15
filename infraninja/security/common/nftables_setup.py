e795a82bd385f762730a3dd97e8c688fa6084899
Mohammad Abu-Khader <mohammad.abukhader@hotmail.com>
2025-05-15 13:53:14 +0400

    refactor: add CommonPackageInstaller class for installing common security packages across Linux distributions
    Signed-off-by: Mohammad Abu-Khader <mohammad.abukhader@hotmail.com>

--- infraninja/security/common/nftables_setup.py
+++ infraninja/security/common/nftables_setup.py
@@ -1,6 +1,6 @@
 from importlib.resources import files as resource_files
-from pyinfra import host
-from pyinfra.api import deploy
+from pyinfra.context import host
+from pyinfra.api.deploy import deploy
 from pyinfra.facts.server import LinuxName
 from pyinfra.operations import files, openrc, server, systemd
 
